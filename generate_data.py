300f04958e17327041f1a08a19bf91dd734557f9
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2024-05-14 02:03:04 +0300

    Generate sample dataset

---
+++ generate_data.py
@@ -0,0 +1,30 @@
+import csv
+import random
+
+SIZE = 10
+MAX_VALUE = 20
+
+def generate():
+    '''
+    data.csv will be used for searching and sorting operations
+    data1.csv and data2.csv will be used for join operation
+    '''
+    with open('data.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'value', 'symbol'])
+        for i in range(SIZE):
+            writer.writerow([i, random.randint(0, MAX_VALUE), chr(random.randint(65, 90))])
+            
+    with open('data1.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'value', 'symbol'])
+        for i in range(SIZE):
+            writer.writerow([i, random.randint(0, MAX_VALUE), chr(random.randint(65, 90))])
+            
+    with open('data2.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'value', 'symbol'])
+        for i in range(SIZE):
+            writer.writerow([i, random.randint(0, MAX_VALUE), chr(random.randint(65, 90))])
+            
+generate()
